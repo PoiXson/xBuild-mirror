7336704c59b9d586f3a659093609fefbd2087b8d
lorenzop <lorenzo@poixson.com>
2021-01-30 18:22:25 -0500

    wrote new autobuild.sh script with more better features

--- src/autobuild.sh
+++ src/autobuild.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 ##===============================================================================
-## Copyright (c) 2019 PoiXson, Mattsoft
+## Copyright (c) 2019-2021 PoiXson, Mattsoft
 ## <https://poixson.com> <https://mattsoft.net>
 ## Released under the GPL 3.0
 ##
-## Description: Script to build rpm's
+## Description: Auto compile a project and build rpm's
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -19,25 +19,204 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ## =============================================================================
-# build-rpm.sh
+# autobuild.sh
+
+source /usr/bin/pxn/scripts/common.sh
+
+
+
+PWD=$(pwd)
+if [ -z $PWD ]; then
+	failure "Failed to find current working directory"
+	exit 1
+fi
+
 
 
 function DisplayHelp() {
-	echo "Usage:"
-	echo "  build-rpm [options] <spec-file>"
+	echo -e "${COLOR_BROWN}Usage:${COLOR_RESET}"
+	echo    "  autobuild [options]"
 	echo
-	echo "Options:"
-	echo "  -n, --build-number             Build number to use with --rpm"
+	echo -e "${COLOR_BROWN}Actions:${COLOR_RESET}"
+	echo -e "  ${COLOR_GREEN}clean${COLOR_RESET}                  Clean the project, removing build files"
+	echo -e "  ${COLOR_GREEN}config${COLOR_RESET}                 "
+	echo -e "  ${COLOR_GREEN}build${COLOR_RESET}                  Compile the project"
+	echo -e "  ${COLOR_GREEN}dist${COLOR_RESET}                   "
 	echo
-	echo "  -C, --no-clear                 Don't clear the screen before doing work"
-	echo "  -h, --help                     Display this help message"
+	echo -e "${COLOR_BROWN}Options:${COLOR_RESET}"
+	echo -e "  ${COLOR_GREEN}-n, --build-number${COLOR_RESET}     Build number to use for packaging"
+	echo                                "                           note: defaults to 'x'"
+	echo -e "  ${COLOR_GREEN}-d, --debug${COLOR_RESET}            Build with debug flags"
+	echo
+	echo -e "  ${COLOR_GREEN}-h, --help${COLOR_RESET}             Display this help message and exit"
+	echo
+	exit 1
+}
+
+
+
+# clean
+function doClean() {
+	title "Clean"
+	did_something=$NO
+	# make clean project
+	if [ -f "$PWD/Makefile.am" ]; then
+		if [ -f "$PWD/Makefile" ]; then
+			\make distclean
+			echo
+			did_something=$YES
+		fi
+		# remove .deps dirs
+		RESULT=$( \find "$PWD" -type d -name .deps )
+		count=0
+		for ENTRY in $RESULT; do
+			[[ -f "$ENTRY" ]] && \rm -v                   "$ENTRY" && count=$((count+1))
+			[[ -d "$ENTRY" ]] && \rm -vrf --preserve-root "$ENTRY" && count=$((count+1))
+		done
+		# remove more files
+		CLEAN_PATHS=". src"
+		CLEAN_FILES="autom4te.cache aclocal.m4 compile configure depcomp install-sh Makefile.in missing"
+		for DIR in $CLEAN_PATHS; do
+			\pushd "$DIR" >/dev/null || continue
+				for ENTRY in $CLEAN_FILES; do
+					[[ -f "$ENTRY" ]] && \rm -v                   "$ENTRY" && count=$((count+1))
+					[[ -d "$ENTRY" ]] && \rm -vrf --preserve-root "$ENTRY" && count=$((count+1))
+				done
+			\popd >/dev/null
+		done
+		if [ $count -gt 0 ]; then
+			echo
+			did_something=$YES
+		fi
+	fi
+	# clean rpm project
+	if [ ! -z $SPEC_FILE ]; then
+		if [ -d "$PWD/rpmbuild" ]; then
+			\rm -vrf --preserve-root rpmbuild
+			echo
+			did_something=$YES
+		fi
+	fi
+	# clean php project
+	if [ -f "$PWD/composer.json" ]; then
+		if [ -d "$PWD/vendor" ]; then
+			\rm -vrf --preserve-root vendor
+			echo
+			did_something=$YES
+		fi
+	fi
+	if [ $did_something -eq $NO ]; then
+		echo "Nothing to do.."
+		echo
+	fi
+}
+
+
+
+# auto configure
+function doConfig() {
+	title "Auto-Configure"
+	if [[ ! -f"$PWD/configure.ac" ]]; then
+		notice "configure.ac file not found, skipping.."
+		echo
+		return
+	fi
+	if [[ ! -f "$PWD/Makefile.am" ]]; then
+		notice "Makefile.am file not found, skipping.."
+		echo
+		return
+	fi
+	\autoreconf -v --install  || exit 1
 	echo
 }
 
 
-YES=1
-NO=0
-NO_CLEAR=$NO
+
+# build
+function doBuild() {
+	title "Build"
+	did_something=$NO
+	if [ -f "$PWD/configure" ]; then
+		if [ $DEBUG -eq $YES ]; then
+			./configure --enable-debug  || exit 1
+		else
+			./configure                 || exit 1
+		fi
+		echo
+	fi
+	if [ -f "$PWD/Makefile" ]; then
+		\make  || exit 1
+		echo
+		did_something=$YES
+	fi
+	if [ $did_something -eq $NO ]; then
+		notice "Nothing found to build.."
+		echo
+	fi
+}
+
+
+
+# distribute
+function doDist() {
+	did_something=$NO
+	# make dist
+	if [ -f "$PWD/Makefile" ]; then
+		title "Distribute"
+		\make dist       || exit 1
+		\make distcheck  || exit 1
+		echo
+		did_something=$YES
+	fi
+	# build rpm
+	if [ ! -z $SPEC_FILE ]; then
+		title "RPM Build"
+		\mkdir -p "$PWD"/rpmbuild/{BUILD,BUILDROOT,SOURCES,SPECS,RPMS,SRPMS,TMP}  || exit 1
+		\cp -vf  "$SPEC_FILE"  "$PWD/rpmbuild/SPECS/"  || exit 1
+		\pushd "$PWD/rpmbuild/" >/dev/null  || exit 1
+			\rpmbuild \
+				${BUILD_NUMBER:+ --define="build_number $BUILD_NUMBER"} \
+				--define="_topdir $PWD" \
+				--define="_tmppath $PWD/TMP" \
+				--define="_binary_payload w9.gzdio" \
+				-bb "SPECS/${SPEC_NAME}.spec" \
+					|| exit 1
+		\popd >/dev/null
+		echo "==============================================="
+		echo "Packages ready for distribution:"
+		\ls -lAsh "$PWD/rpmbuild/RPMS/"  || exit 1
+		echo "==============================================="
+		echo
+		did_something=$YES
+	fi
+	if [ $did_something -eq $NO ]; then
+		title "Distribute"
+		echo "Nothing to do.."
+		echo
+	fi
+}
+
+
+
+# find .spec file
+SPEC_FILE_COUNT=$(\ls -1 "$PWD/"*.spec 2>/dev/null | \wc -l)
+if [ $SPEC_FILE_COUNT -eq 1 ]; then
+	SPEC_FILE=$(\ls -1 "$PWD/"*.spec)
+	SPEC_NAME="${SPEC_FILE%%.*}"
+	SPEC_NAME="${SPEC_NAME##*/}"
+elif [ $SPEC_FILE_COUNT -gt 1 ]; then
+	warning "$SPEC_FILE_COUNT .spec files were found here"
+fi
+
+
+
+# parse args
+echo
+if [ $# -eq 0 ]; then
+	DisplayHelp
+	exit 1
+fi
+ACTIONS=""
 while [ $# -gt 0 ]; do
 	case "$1" in
 	# build number
@@ -48,80 +227,54 @@ while [ $# -gt 0 ]; do
 	--build-number=*)
 		BUILD_NUMBER="${1#*=}"
 	;;
-	# don't clear screen
-	-C|--no-clear)
-		NO_CLEAR=$YES
+	# debug mode
+	-d|--debug)
+		DEBUG=$YES
 	;;
 	# display help
 	-h|--help)
-		if [ $NO_CLEAR -ne $YES ]; then
-			\clear
-		fi
+		DisplayHelp
+		exit 1
+	;;
+	-*)
+		failure "Unknown argument: $1"
+		echo
 		DisplayHelp
 		exit 1
 	;;
 	*)
-		if [ ! -z $SPEC_NAME ]; then
-			echo "Provide only one spec name to build."
-			exit 1
-		fi
-		SPEC_NAME="$1"
+		ACTIONS="$ACTIONS $1"
 	;;
 	esac
 	\shift
 done
 
 
-if [ $NO_CLEAR -ne $YES ]; then
-	\clear
-fi
+
+# perform actions
+for ACT in $ACTIONS; do
+	case "$ACT" in
+	clean)
+		doClean
+	;;
+	config)
+		doConfig
+	;;
+	build)
+		doBuild
+	;;
+	dist)
+		doDist
+	;;
+	*)
+		failure "Unknown action: $ACT"
+		echo
+		exit 1
+	;;
+	esac
+done
 
 
-if [ -z $SPEC_NAME ]; then
-	SPEC_FILES_FOUND=$( \ls -1 *.spec )
-	SPEC_FILES_COUNT=$( echo "$SPEC_FILES_FOUND" | wc -l )
-	if [ $SPEC_FILES_COUNT -eq 1 ]; then
-		SPEC_NAME="$SPEC_FILES_FOUND"
-	fi
-fi
-if [ -z $SPEC_NAME ]; then
-	echo
-	echo "Provide the spec name to build."
-	echo
-	DisplayHelp
-	exit 1
-fi
-SPEC_NAME="${SPEC_NAME%%.*}"
 
-
-# prepare build space
-PWD=$(pwd)
-if [ -d "$PWD/rpmbuild/" ]; then
-	\rm -Rf --preserve-root "$PWD/rpmbuild/"  || exit 1
-	echo
-fi
-\mkdir -p "$PWD"/rpmbuild/{BUILD,BUILDROOT,SOURCES,SPECS,RPMS,SRPMS,TMP}  || exit 1
-#rpmdev-setuptree  || exit 1
-
-
-\cp -fv  "$SPEC_NAME.spec"  "$PWD/rpmbuild/SPECS/"  || exit 1
-echo
-
-
-echo 'Building...'
-\pushd "$PWD/rpmbuild/"  || exit 1
-	\rpmbuild \
-		${BUILD_NUMBER:+ --define="build_number ${BUILD_NUMBER}"} \
-		--define="_topdir $PWD" \
-		--define="_tmppath $PWD/TMP" \
-		--define="_binary_payload w2.xzdio" \
-		-bb "SPECS/$SPEC_NAME.spec" \
-			|| exit 1
-\popd
-
-
-echo;echo
-echo "Success!"
-\ls -lAsh "$PWD/rpmbuild/RPMS/"  || exit 1
-echo;echo
+echo -e "${COLOR_BROWN}Done${COLOR_RESET}"
 exit 0
