f13d83d3f0bde7ebafa3210ae37b2ddf72e84ced
lorenzop <lorenzo@poixson.com>
2021-03-09 20:59:45 -0500

    added phpunit to project-tools-aliases.sh

--- src/project-tools-aliases.sh
+++ src/project-tools-aliases.sh
@@ -4,3 +4,6 @@
 alias autobuild='clear;autobuild'
 alias workspace='clear;workspace'
 alias genautotools='clear;genautotools'
+
+alias phpunit='vendor/bin/phpunit -v --coverage-html coverage/html'
+alias phpunit='clear;phpunit'
